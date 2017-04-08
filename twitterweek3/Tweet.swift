d075534938dc03274d99e46e14739102baa51709
Victoria <victoria.ku@airbnb.com>
2017-04-08 11:30:59 -0700

    second push

---
+++ twitterweek3/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  twitterweek3
+//
+//  Created by Victoria Ku on 4/5/17.
+//  Copyright © 2017 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ twitterweek3/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  twitterweek3
+//
+//  Created by Victoria Ku on 4/5/17.
+//  Copyright © 2017 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
